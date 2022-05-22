fe8d714224e16830d5925b4b74608b71f28f0888
gftea <ckq614@gmail.com>
2022-05-22 15:58:47 +0200

    add macro crate template

--- mybinary/src/main.rs
+++ mybinary/src/main.rs
@@ -2,8 +2,17 @@ use mylib1;
 use mylib2;
 use rand;
 
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)] // generate implementation of trait HelloMacro for Pancakes
+struct Pancakes;
+
 fn main() {
     println!("workspace rand: {}", rand::random::<i32>());
     println!("access {}", mylib1::who_am_i());
     println!("access {}", mylib2::who_am_i());
+
+    // call the generated implementation of trait HelloMacro
+    Pancakes::hello_macro();
 }
